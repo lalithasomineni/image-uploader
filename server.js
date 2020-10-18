239ea553668ffee9d17cb1286ef81b9ce2f528a6
lalli <lsomineni@gmail.com>
2020-10-18 09:26:44 +0530

    errors rectified

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const port = 3000;
-
 const mongoose = require("mongoose");
- const mongodburl = process.env.url;
+const mongodburl = process.env.mongodburl||"mongodb://localhost/image_uploader";
 
 mongoose
-  .connect(mongodburl)
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error(err)),{ useNewUrlParser: true };
-
+ .connect(mongodburl)
+ .then(() => console.log("Connected to MongoDB..."))
+ .catch((err) => console.error(err));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: false
