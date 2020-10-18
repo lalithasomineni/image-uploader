6a3421f86b1e76f5cc5d8a255ad23f041bce3fcf
lalli <lsomineni@gmail.com>
2020-10-18 10:16:35 +0530

    updated

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const bodyparser = require("body-parser");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-const mongodburl = process.env.mongodburl||"mongodb://localhost/image_uploader";
+const mongodburl = process.env.MONGODB_URI||"mongodb+srv://user:12345@cluster0.70k9n.gcp.mongodb.net/image_uploader?retryWrites=true&w=majority";
 
 mongoose
- .connect(mongodburl)
- .then(() => console.log("Connected to MongoDB..."))
- .catch((err) => console.error(err));
+  .connect(mongodburl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error(err));
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: false
