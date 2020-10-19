f2f07bb094c1acd5874d695b1bdb856e8beefb35
lalli <lsomineni@gmail.com>
2020-10-19 09:25:46 +0530

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
+//const swaggerjsdoc = require("swagger-jsdoc");
+//const swaggerui = require("swagger-ui-express");
+//const YAML = require("yamljs");
+//const swaggerDocument = YAML.load("./swagger.yaml");
 //const port = process.env.port||3000;
 const mongoose = require("mongoose");
 const mongodburl = process.env.mongodburl||"mongodb+srv://user:12345@cluster0.mmisy.mongodb.net/<dbname>?retryWrites=true&w=majority"
@@ -15,12 +19,10 @@ app.use(bodyparser.urlencoded({
 }));
 
 
+
 app.use("/api/users",require("./routes/user"));
 app.use("/api/images",require("./routes/image"));
 
 app.listen(process.env.PORT || 5000,()=>{
   console.log("server started...");
 })
-app.get("/", (req, res) => {
-  res.send("<h1>freelancing<h1>");
-});
